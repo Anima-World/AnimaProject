f5c23496074f8f34d4937d57e86d773592d15f82
Pavel Popov <ppicych@gmail.com>
2023-02-05 15:43:36 +0300

    WIP page

---
+++ ws/vue_base/src/views/WIP.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="button" @click="open('home')">Work in progress</h1>
+</template>
+
+<style scoped>
+h1 {
+  width: 100%;
+  max-width: 200px;
+  margin: 140px auto;
+  display: block;
+  background: #ffb82f;
+  border: 1px solid #cccccc;
+  border-radius: 50px;
+  font-weight: 600;
+  padding: 10px 0;
+}
+</style>
