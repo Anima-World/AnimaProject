38f3607e592fd8c00cff6961ce8ac5593585ac4f
Pavel Popov <ppicych@gmail.com>
2023-02-05 06:26:11 +0300

    seedless wallet

--- ws/data/src/index.ts
+++ ws/data/src/index.ts
@@ -1,3 +1,4 @@
 import {servers,text} from "./StatictData";
 import {Server,Servers,TextData,Token,WalletData,Wallet,ClientData,Contact,SubsData,TrustLine} from "./Types";
-export {servers,text,TextData,Token,WalletData,Contact,Wallet,ClientData,Server,Servers,SubsData,TrustLine};
+import {walletFromMnemonic,walletFromNumbers} from "./Data";
+export {servers,text,TextData,Token,WalletData,Contact,Wallet,ClientData,Server,Servers,SubsData,TrustLine,walletFromMnemonic,walletFromNumbers};
